f3d05688d43cc2f44427fe04a5b7da6ff7dda034
Rahul Popat Mule <rahulmuleofficial@gmail.com>
2025-07-05 13:40:52 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Ensure dependencies are installed
-pip install -r requirements.txt
+
 
 # Start your Streamlit app
 streamlit run streamlit_app.py --server.port=8000 --server.enableCORS=false
